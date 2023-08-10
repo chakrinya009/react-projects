297e09aeb4a4b3de3364851e5afc591bc664b555
chakrinya <chakrinyabalabadra@gmail.com>
2023-08-10 20:18:25 +0530

    redux

--- shopvista/src/App.js
+++ shopvista/src/App.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import Header from "./containers/Header";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ProductListing from "./containers/ProductListing";
+import ProductDetails from "./containers/ProductDetails";
+import './App.css'
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<ProductListing />} />
+          <Route path="/product/:productid" element={<ProductDetails />} />
+          <Route>404 NOT FOUND</Route>
+        </Routes>
+      </Router>
+    </div>
+  );
+};
 
-export default App
+export default App;
